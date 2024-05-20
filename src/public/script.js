4524f926b5ed5b2f00f824bd44492a4d8df49d7c
haecheonlee <dlgocjs1@gmail.com>
2024-05-19 20:03:03 -0400

    Revert "Fix ui of corners with piece"
    This reverts commit 8f00d726ce56b98db9132ba0a296aa0a1079778d.

--- src/public/script.js
+++ src/public/script.js
@@ -160,9 +160,9 @@ document.addEventListener("DOMContentLoaded", () => {
             validMoves.forEach((move) => {
                 const square = getSquareElement(move);
                 if (square) {
-                    const hasPiece =
-                        square.firstChild?.classList.contains("piece");
-                    square.classList.add(hasPiece ? "corners" : "pointer");
+                    square.classList.add(
+                        square.hasChildNodes() ? "corners" : "pointer"
+                    );
                 }
             });
         });
